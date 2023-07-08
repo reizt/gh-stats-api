6465c10fa4a0613c8bf353de5fb0c3015ccaa88d
reizt <reizt.dev@gmail.com>
2023-07-08 20:28:06 +0900

    esbuildからvanilla-extractを削除

--- esbuild.js
+++ esbuild.js
@@ -1,13 +1,11 @@
 const { build } = require('esbuild');
 const { resolve } = require('path');
-const { vanillaExtractPlugin } = require('@vanilla-extract/esbuild-plugin');
 
 /** @type {import('esbuild').BuildOptions} */
 const opts = {
   tsconfig: resolve(__dirname, './tsconfig.json'),
   platform: 'node',
   define: { 'process.env.NODE_ENV': `"${process.env.NODE_ENV}"` },
-  plugins: [vanillaExtractPlugin()],
   color: true,
   bundle: true,
 };
@@ -18,13 +16,11 @@ if (env === 'development') {
   opts.outfile = resolve(__dirname, './dist/development.js');
   opts.minify = false;
   opts.sourcemap = true;
-  // opts.external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
 } else if (env === 'production') {
   opts.entryPoints = [resolve(__dirname, './src/tasks/vercel.ts')];
   opts.outfile = resolve(__dirname, './api/production.js');
   opts.minify = true;
   opts.sourcemap = false;
-  // opts.external = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
 } else {
   throw new Error(`Invalid env: ${env}`);
 }
