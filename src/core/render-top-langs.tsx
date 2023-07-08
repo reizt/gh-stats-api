a4c3b574170ac0d5dd2c87d0a77fe762ab9ecb7c
reizt <reizt.dev@gmail.com>
2023-07-08 19:35:08 +0900

    satoriでSVGに変換

--- src/core/render-top-langs.tsx
+++ src/core/render-top-langs.tsx
@@ -1,27 +1,28 @@
 import * as React from 'react';
-import ReactDOMSserver from 'react-dom/server';
 import type { LangStat } from './fetch-top-langs';
 
 type Props = {
   userName: string;
   langs: LangStat[];
+  theme: 'light' | 'dark';
 };
 
-const TopLangsSVG: React.FC<Props> = ({ userName, langs }) => {
+const TopLangsSVG: React.FC<Props> = ({ userName, langs, theme }) => {
   return (
     <div
       style={{
-        fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI","Noto Sans",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
         width: '300px',
+        display: 'flex',
+        flexDirection: 'column',
       }}
     >
-      <style>
+      {/* <style>
         {`
           .lang-link:hover, .lang-link:hover span {
             color: #2f81f7 !important;
           }
         `}
-      </style>
+      </style> */}
       <div style={{ display: 'flex', marginBottom: '10px', height: '8px', borderRadius: '4px', overflow: 'hidden' }}>
         {langs.map((lang) => (
           <div key={lang.name} style={{ width: `${lang.rate * 100}%`, height: '100%', backgroundColor: lang.color }}></div>
@@ -33,13 +34,30 @@ const TopLangsSVG: React.FC<Props> = ({ userName, langs }) => {
             href={`https://github.com/search?q=user%3A${userName}++language%3A${lang.name}&type=code`}
             key={lang.name}
             className="lang-link"
-            style={{ display: 'flex', alignItems: 'center', columnGap: '5px', color: '#fff', textDecoration: 'none' }}
+            style={{ display: 'flex', alignItems: 'center', columnGap: '5px', textDecoration: 'none' }}
           >
             <span style={{ width: '16px', height: '16px', display: 'flex', justifyContent: 'center', alignItems: 'center', marginRight: '8px' }}>
               <span style={{ width: '8px', height: '8px', backgroundColor: lang.color, borderRadius: '100%' }} />
             </span>
-            <span style={{ fontWeight: 'bold', marginRight: '4px' }}>{lang.name}</span>
-            <span style={{ color: '#7d8590' }}>{`${(lang.rate * 100).toFixed(1)}%`}</span>
+            <span
+              className="lang-name"
+              style={{
+                fontFamily: 'Roboto',
+                fontWeight: 'bold',
+                color: theme === 'light' ? '#24292f' : '#fff',
+                marginRight: '4px',
+              }}
+            >
+              {lang.name}
+            </span>
+            <span
+              className="lang-rate"
+              style={{
+                fontFamily: 'Roboto',
+                fontWeight: 'normal',
+                color: theme === 'light' ? '#57606a' : '#7d8590',
+              }}
+            >{`${(lang.rate * 100).toFixed(1)}%`}</span>
           </a>
         ))}
       </div>
@@ -47,6 +65,6 @@ const TopLangsSVG: React.FC<Props> = ({ userName, langs }) => {
   );
 };
 
-export const renderTopLangs = (userName: string, langs: LangStat[]): string => {
-  return ReactDOMSserver.renderToString(<TopLangsSVG userName={userName} langs={langs} />);
+export const renderTopLangs = (props: Props) => {
+  return <TopLangsSVG {...props} />;
 };
--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,6 +1,9 @@
 import { fetchTopLangs } from '#/core/fetch-top-langs';
 import { renderTopLangs } from '#/core/render-top-langs';
+import axios from 'axios';
 import express from 'express';
+import ReactDOMServer from 'react-dom/server';
+import satori from 'satori';
 import { z } from 'zod';
 
 const app = express();
@@ -13,18 +16,64 @@ const api = express.Router();
 const querySchema = z.object({
   userName: z.coerce.string(),
   limit: z.coerce.number().int().optional(),
+  theme: z.enum(['light', 'dark']).optional(),
 });
 
-api.get('/top-langs', async (req, res) => {
+const handleError = (err: any, res: express.Response) => {
+  if (err instanceof z.ZodError) {
+    console.log(err.issues);
+    res.status(400).send('Bad Request');
+    return;
+  } else if (err instanceof Error) {
+    console.log(err.message);
+  } else {
+    console.log(err);
+  }
+  res.status(500).send('Internal Server Error');
+};
+
+api.get('/top-langs.svg', async (req, res) => {
   try {
-    const { userName, limit } = querySchema.parse(req.query);
+    const { userName, limit, theme } = querySchema.parse(req.query);
     const langs = await fetchTopLangs({ userName, limit });
-    const svg = renderTopLangs(userName, langs);
-    res.setHeader('Content-Type', 'text/html');
+    const reactNode = renderTopLangs({ userName, langs, theme: theme ?? 'light' });
+    const { data: robotoRegular } = await axios.get('https://d3qgfj7bktqmwv.cloudfront.net/Roboto-Regular.ttf', { responseType: 'arraybuffer' });
+    const { data: robotoBold } = await axios.get('https://d3qgfj7bktqmwv.cloudfront.net/Roboto-Bold.ttf', { responseType: 'arraybuffer' });
+    const svg = await satori(reactNode, {
+      fonts: [
+        {
+          name: 'Roboto',
+          data: robotoRegular,
+          weight: 400,
+          style: 'normal',
+        },
+        {
+          name: 'Roboto',
+          data: robotoBold,
+          weight: 700,
+          style: 'normal',
+        },
+      ],
+      width: 320,
+      height: 200,
+    });
+    res.setHeader('Content-Type', 'image/svg+xml');
     res.send(svg);
   } catch (err) {
-    console.log(err);
-    res.status(500).send('Internal Server Error');
+    handleError(err, res);
+  }
+});
+
+api.get('/top-langs.html', async (req, res) => {
+  try {
+    const { userName, limit, theme } = querySchema.parse(req.query);
+    const langs = await fetchTopLangs({ userName, limit });
+    const reactNode = renderTopLangs({ userName, langs, theme: theme ?? 'light' });
+    const html = ReactDOMServer.renderToString(reactNode);
+    res.setHeader('Content-Type', 'text/html');
+    res.send(html);
+  } catch (err) {
+    handleError(err, res);
   }
 });
 
