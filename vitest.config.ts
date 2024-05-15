1e792826ec08bfb8b6558c70ac9afa209aaa6d9b
Reiju Takahashi <reizt.dev@gmail.com>
2024-05-16 01:16:24 +0900

    Add E2E test

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,14 +4,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		globals: true,
-		include: ['src/**/*.test.ts'],
-		coverage: {
-			provider: 'v8',
-		},
-	},
-	resolve: {
-		alias: {
-			'~': './src',
-		},
+		include: ['e2e/*.test.ts'],
 	},
 });
