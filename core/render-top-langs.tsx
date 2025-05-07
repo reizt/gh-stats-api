7cf94ed12833e8c9f595d4e704f4d5a68f425b51
Reiju Takahashi <reizt.dev@gmail.com>
2025-05-07 12:22:26 +0900

    👍 show details of font fetching error

--- core/render-top-langs.tsx
+++ core/render-top-langs.tsx
@@ -63,6 +63,7 @@ const TopLangsSVG: React.FC<Props> = ({ username, langs, theme }) => {
 const fetchFont = async (url: string) => {
 	const response = await fetch(url);
 	if (!response.ok) {
+		console.log(response.status, await response.text());
 		throw new Error(`Failed to fetch font from ${url}`);
 	}
 	const buffer = await response.arrayBuffer();
